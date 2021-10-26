2d9b328ebe049129c96f6a3f9f0a5e6dc4f71efc
Amargupta12345 <53191408+Amargupta12345@users.noreply.github.com>
2021-10-26 22:37:25 +0530

    done

---
+++ src/constant.js
@@ -0,0 +1,16 @@
+const REACT_APP_MAPBOX =
+  "pk.eyJ1IjoiamFydmlzMTIzNDUiLCJhIjoiY2t1emZpNzk4MDQyYjJvcGEyN29xcW16aSJ9.DQxiEQCBDH7x4LIPe1vCDA";
+const REACT_MAP_URL = "mapbox://styles/jarvis12345/ckuzmaehk0yms15mvrnmgx992";
+
+const REACT_APP_GOOGLE_MAP_API_KEY =
+  "AIzaSyAA8KMSH5IrzaVgu97S - brl3X6fcTKvIws";
+
+const REACT_APP_RAPID_API_KEY =
+  "0ff949cc99mshf6182dfb8cb9e55p15c89bjsn70c71b5ff51d";
+
+module.exports = {
+  REACT_APP_MAPBOX,
+  REACT_MAP_URL,
+  REACT_APP_GOOGLE_MAP_API_KEY,
+  REACT_APP_RAPID_API_KEY,
+};
